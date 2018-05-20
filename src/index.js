f013ff5b58255d82663155586fbd01bf135dc647
Subrat <06.subrata@gmail.com>
2018-05-20 23:27:19 +0530

    Pre courses.json structure modification commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import path from 'path';
 import CourseScraper from './courseScraper';
 import CourseDownloader from './courseDownloader';
 
-const courseName = '';
-new CourseScraper(courseName).boot({
-  headless: true,
-  userDataDir: path.join(__dirname, '../puppeteer-data-dir'),
-  executablePath: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
-}, {
-  width: 1500,
-  height: 1024,
-  setBypassCSP: true,
-});
+const courseName = 'node-js-essential-training';
+(async () => {
+  new CourseScraper(courseName).boot({
+    headless: true,
+    userDataDir: path.join(__dirname, '../puppeteer-data-dir'),
+    executablePath: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+  }, {
+    width: 1500,
+    height: 1024,
+    setBypassCSP: true,
+  });
 
-new CourseDownloader(courseName).downloadAll()
-  .then(dir => console.log(`All courses in the course '${CourseDownloader}' downloaded @${dir}.`));
+  await new CourseDownloader(courseName).downloadAll()
+    .then(dir => console.log(`All courses in the course '${CourseDownloader}' downloaded @${dir}.`));
+})();
